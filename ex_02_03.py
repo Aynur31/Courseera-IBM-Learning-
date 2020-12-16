e90fab1ed739258ec6a3d680dc677a8e83fbbda2
Aynur31 <68058339+Aynur31@users.noreply.github.com>
2020-12-16 15:05:47 +0300

    Add files via upload

---
+++ ex_02_03.py
@@ -0,0 +1,13 @@
+## belwo code was from the video
+hrs = input('Enter hours: ')
+rate = input('Enter rate:')
+pay=float(hrs)*float(rate)
+print("Pay: ", pay)
+### below code i made myslef as input function is always string need to make it
+## integer or float
+hours = input('Enter Hours:')
+rate = input('Enter rate:')
+hrs=int(hours)
+rate=float(rate)
+pay = hrs*rate
+print('Pay: ',pay)
